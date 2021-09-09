f9040909217a2b0c0fd7be1a6a55c4428b4f05df
Bruno Miquelini Dos Santos <bsmiquelini@cast.fazenda.sp.gov.br>
2021-09-09 19:24:17 +0000

    Updated Startup.cs

--- modelo_core_mvc/Startup.cs
+++ modelo_core_mvc/Startup.cs
@@ -40,8 +40,7 @@ namespace modelo_core_mvc
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
         public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
         {
-            //if (env.IsDevelopment())
-            if (Environment.IsEnvironment("Production"))
+            if (env.IsDevelopment())
             {
                 app.UseDeveloperExceptionPage();
             }
