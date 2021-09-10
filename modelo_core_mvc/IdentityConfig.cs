cc4bcb78b446c025d2d1b1a6e36da043511641a3
Walter de Tarso <wtfscampos@fazenda.sp.gov.br>
2021-09-10 15:00:05 -0300

    Alteração de "issuer" para "metadataaddress"

---
+++ modelo_core_mvc/IdentityConfig.cs
@@ -0,0 +1,107 @@
+﻿using Microsoft.AspNetCore.Http;
+using System;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Authentication.WsFederation;
+using Microsoft.Extensions.Configuration;
+using Microsoft.AspNetCore.Authentication.Cookies;
+using Microsoft.IdentityModel.Tokens;
+
+namespace Identity
+{
+    public class IdentityConfig
+    {
+        private static IConfiguration configuration;
+
+        public static Action<WsFederationOptions> WSFederationOptions { get; private set; }
+        public static Action<CookieAuthenticationOptions> CookieAuthenticationOptions { get; private set; }
+        public static Action<Microsoft.AspNetCore.Authentication.AuthenticationOptions> AuthenticationOptions { get; private set; }
+
+        public IdentityConfig()
+        {
+        }
+
+        public static void RegistrarOpcoes(IConfiguration Configuration)
+        { 
+            AuthenticationOptions = options =>
+            {
+                configuration = Configuration;
+                options.DefaultScheme = CookieAuthenticationDefaults.AuthenticationScheme;
+                options.DefaultChallengeScheme = WsFederationDefaults.AuthenticationScheme;
+            };
+
+            WSFederationOptions = options =>
+            {
+                options.Wtrealm = configuration["identity:realm"];
+                options.Wreply  = configuration["identity:reply"];
+                options.MetadataAddress = configuration["identity:metadataaddress"];
+                options.Events.OnRedirectToIdentityProvider = OnRedirectToIdentityProvider;
+                options.Events.OnSecurityTokenReceived = OnSecurityTokenReceived;
+                options.TokenValidationParameters = new TokenValidationParameters { SaveSigninToken = true };
+                options.CorrelationCookie = new CookieBuilder
+                {
+                    Name = ".Correlation.",
+                    HttpOnly = true,
+                    IsEssential = true,
+                    SameSite = SameSiteMode.None,
+                    SecurePolicy = CookieSecurePolicy.Always,
+                    Expiration = new TimeSpan(0, 0, 15, 0),
+                    MaxAge = new TimeSpan(0, 0, 15, 0),
+                };
+            };
+
+            CookieAuthenticationOptions = options =>
+            {
+                options.Cookie = new CookieBuilder
+                {
+                    Name = "FedAuth",
+                    HttpOnly = true,
+                    IsEssential = true,
+                    SameSite = SameSiteMode.None,
+                    SecurePolicy = CookieSecurePolicy.Always,
+                };
+                options.ExpireTimeSpan = new TimeSpan(0, 0, int.Parse(configuration["identity:timeout"]), 0);
+                options.SlidingExpiration = false;
+            };
+        }
+
+        private static async Task<Task<int>> OnSecurityTokenReceived(SecurityTokenReceivedContext arg)
+        {
+            TokenWSClient tokenWS = new TokenWSClient(TokenWSClient.EndpointConfiguration.TokenWS, configuration["identity:tokenws"]);
+            try
+            {
+                if (await tokenWS.IsTokenValidAsync(arg.ProtocolMessage.GetToken(), configuration["identity:realm"], "00031C33"))
+                {
+                    return Task.FromResult(0);
+                }
+            }
+            finally
+            {
+                #region Close_or_Abort
+                if (tokenWS != null)
+                {
+                    try
+                    {
+                        await tokenWS.CloseAsync();
+                    }
+                    catch (Exception)
+                    {
+                        tokenWS.Abort();
+                    }
+                }
+                #endregion
+            }
+            throw new Exception($"Token recebido é inválido ou não foi emitido para '{configuration["identity:realm"]}'.");
+        }
+
+        public static Task OnRedirectToIdentityProvider(RedirectContext arg)
+        {
+            arg.ProtocolMessage.Wauth = configuration["identity:Wauth"];
+            arg.ProtocolMessage.Wfresh = configuration["identity:timeout"];
+            arg.ProtocolMessage.Parameters.Add("ClaimSets", "80000000");
+            arg.ProtocolMessage.Parameters.Add("TipoLogin", "00031C33");
+            arg.ProtocolMessage.Parameters.Add("AutoLogin", "0");
+            arg.ProtocolMessage.Parameters.Add("Layout", "2");
+            return Task.FromResult(0);
+        }
+    }
+}
---
+++ modelo_core_mvc/Usuario.cs
@@ -0,0 +1,99 @@
+﻿using Microsoft.AspNetCore.Http;
+using System;
+using System.Linq;
+using System.Text;
+using System.Xml;
+using Microsoft.Extensions.Configuration;
+
+namespace Identity
+{
+    public class Usuario
+    {
+        public string Token { get; set; }
+        public string Login { get; set; }
+        public string Nome { get; set; }
+        public string DocumentoIdentificacao { get; set; }
+        public string DataNascimento { get; set; }
+        private IHttpContextAccessor Acessor { get; }
+        private static IConfiguration Configuration { get; set; }
+        private XmlDocument TokenSAML { get; set; }
+
+        public Usuario(IHttpContextAccessor acessor, IConfiguration configuration)
+        {
+            Acessor = acessor;
+            Configuration = configuration;
+            TokenSAML = new XmlDocument();
+            if (Acessor.HttpContext.User.Identities.First().BootstrapContext != null)
+            {
+                Token = Acessor.HttpContext.User.Identities.First().BootstrapContext.ToString();
+                //Se BootstrapContext tem conteúdo, estamos no contexto da autenticação na aplicação
+                TokenSAML.LoadXml(Token);
+                Token = Convert.ToBase64String(System.Text.UTF8Encoding.UTF8.GetBytes(Token));
+            }
+            else
+            {
+                //Se não, estamos tratando da autorização que chegou na requisição à api
+                if (Acessor.HttpContext.Request.Headers["Authorization"].Count > 0)
+                {
+                    try
+                    {
+                        TokenSAML.LoadXml(UTF8Encoding.UTF8.GetString(Convert.FromBase64String(Acessor.HttpContext.Request.Headers["Authorization"].ToString().Split(" ")[1])));
+                    }
+                    catch
+                    {
+
+                    }
+                }
+            }
+            RegistrarPropriedades();
+        }
+
+        private void RegistrarPropriedades()
+        {
+            if (TokenSAML != null)
+            {
+                var _atributos = TokenSAML.GetElementsByTagName("saml:Attribute");
+                var _ver1 = (_atributos.Count > 0);
+                if (!_ver1) { _atributos = TokenSAML.GetElementsByTagName("Attribute"); }
+
+                string _campo;
+                string _valor;
+                foreach (XmlNode _atributo in _atributos)
+                {
+                    if (_ver1)
+                    {
+                        _campo = _atributo.Attributes.GetNamedItem("AttributeName").InnerText;
+                    }
+                    else
+                    {
+                        _campo = _atributo.Attributes[0].Value;
+                        _campo = _campo.Split("/")[_campo.Split("/").Count() - 1];
+                    }
+
+                    _valor = _atributo.InnerText;
+
+                    if (_campo == "upn")
+                        Login = _valor;
+                    else
+                    if (_campo == "name")
+                    {
+                        Nome = _valor.Split(":")[0];
+                        if (_valor.Split(":").Count() > 1)
+                        {
+                            DocumentoIdentificacao = _valor.Split(":")[1];
+                        }
+                    }
+                    else
+                    if (_campo == "CNPJ")
+                        DocumentoIdentificacao = _valor;
+                    else
+                    if (_campo == "CPF")
+                        DocumentoIdentificacao = _valor;
+                    else
+                    if (_campo == "dateofbirth")
+                        DataNascimento = _valor;
+                };
+            }
+        }
+    }
+}
