6d297e2d9d74c5dc9c864fbc74277ff2f68b08f6
Walter de Tarso <wtfscampos@fazenda.sp.gov.br>
2021-11-11 11:13:33 -0300

    appinsights

--- modelo_core_mvc/Startup.cs
+++ modelo_core_mvc/Startup.cs
@@ -41,6 +41,9 @@ namespace modelo_core_mvc
             services.AddTransient<Usuario>();
 
             services.AddHttpClient<ProjetosApiClient>();
+            services.AddApplicationInsightsTelemetry(Configuration["APPINSIGHTS_CONNECTIONSTRING"]);
+            services.AddApplicationInsightsTelemetry();
+            services.AddApplicationInsightsTelemetry(Configuration["APPINSIGHTS_CONNECTIONSTRING"]);
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
